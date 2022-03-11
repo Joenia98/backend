9a43f0b6863c9a120d9cd39f9430808327bca047
Joenia98 <josegumartmtz@gmail.com>
2022-03-10 20:43:35 -0600

    "Instalacion de base de datos alojada"

--- db/index.js
+++ db/index.js
@@ -1,8 +1,7 @@
 const mongoose = require("mongoose");
 
 const connectDB = async () => {
-  mongoose.connect("mongodb://localhost/employees");
-  console.log("MongoDB Conected");
+  mongoose.connect("mongodb+srv://josegumart:bs7koFBKfHYBa3Bw@cluster0.a0vkv.mongodb.net/employees?retryWrites=true&w=majority");
 };
 
 module.exports = { connectDB };
